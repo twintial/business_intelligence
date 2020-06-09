0e9db8e87083f86362ae562669f8fa75d2789fd0
hailuo <550847434@qq.com>
2020-06-09 21:49:51 +0800

    first commit

---
+++ src/main/java/com/bi/dbpedia/DbpediaApplication.java
@@ -0,0 +1,13 @@
+package com.bi.dbpedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbpediaApplication.class, args);
+    }
+
+}
